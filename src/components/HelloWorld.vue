d9ae0e9301cb98a0ef7a18648f3b6b900bda1394
Jorgeeperalta <edudela36@gmail.com>
2023-08-22 12:02:41 -0300

    plantilla funcionando

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref } from 'vue'
-import { useStore } from '../store'
+import { useStore } from '../store/useStore'
 
 const counter = useStore()
 
@@ -12,33 +12,59 @@ const count = ref(0)
 </script>
 
 <template>
+ 
+  <v-sheet
+    elevation="12"
+    max-width="600"
+    rounded="lg"
+    width="100%"
+    class="pa-4 text-center mx-auto"
+  >
+    <v-icon
+      class="mb-5"
+      color="success"
+   
+      size="112"
+    >mdi-check-circle</v-icon>
+
+    <h2 class="text-h5 mb-6">You reconciled this account</h2>
+
+    <p class="mb-4 text-medium-emphasis text-body-2">
+      To see a report on this reconciliation, click <a href="#" class="text-decoration-none text-info">View reconciliation report.</a>
+
+      <br>
+
+      Otherwise, you're done!
+    </p>
+
+    <v-divider class="mb-4"></v-divider>
+
+    <div class="text-end">
+      <v-btn
+        class="text-none"
+        color="success"
+        rounded
+        variant="flat"
+        width="90"
+      >
+        Done
+      </v-btn>
+    </div>
+  </v-sheet>
+
   <h1>{{ msg }} {{ counter.name }} {{counter.count}}</h1>
 
 
   <div class="card">
-    <button type="button" @click="counter.count++">count is {{ counter.count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <v-btn type="button" @click="counter.count++">count is {{ counter.count }}</v-btn>
+   
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+ 
 </template>
 
 <style scoped>
 .read-the-docs {
-  color: #888;
+  color: #ac3a3a;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import '@mdi/font/css/materialdesignicons.css'
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 const pinia = createPinia()
 const app = createApp(App)
-
+app.use(vuetify)
 app.use(pinia)
 app.mount('#app')
